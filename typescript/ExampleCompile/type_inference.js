0a19ec5d7e65e63db1effac7d6aeb2f1ae66ea45
Andre Paradise <portland_state@yahoo.com>
2018-04-22 02:59:52 -0700

    updated typescript notes.

---
+++ typescript/ExampleCompile/type_inference.js
@@ -0,0 +1,8 @@
+var car = {
+    brand: 'BMW',
+    engine: 3.4,
+    run: function () {
+        console.log(" i am driging 100ph");
+    }
+};
+car.brand = '5.5'; //i detete the quotes i will get typscript error asking for a string
