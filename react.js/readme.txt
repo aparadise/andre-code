eb62c64bdcc1cbf661ece190f014e66be975ac7d
LAPTOP-3KU7C2B9\Juice1 <portland_state@yahoo.com>
2018-10-06 17:29:03 -0700

    added react.js with sql for using db4free.net  free sql for fake shopping cart for V1 test. and java files

---
+++ react.js/readme.txt
@@ -0,0 +1,56 @@
+
+http://localhost:3000/ (frontend this will start depening on what server is open)
+
+http://localhost:3000/admin
+now its admin@admin.com psw: admin
+
+-----------------
+the sql is working with http://docs.sequelizejs.com/
+-------------------------
+For Demo project we have two different repos
+
+
+
+
+
+Prerequisites -
+
+Install node.js - https://nodejs.org/en/
+
+1) ecomm-api is node project for BackEnd to run server run following command -
+
+npm install then
+
+node server.js
+
+
+
+2) reactEcommFrontEnd is a front End app built on React.js
+
+to run this app run follwing comand -
+
+npm install then
+
+npm start
+
+
+
+open url - http://localhost:3000 on browser .
+
+
+
+note: the database is being hosted on db4free.net
+
+create a database
+
+user/psw
+
+dbInit.js
+
+backEnd
+
+
+*************************
+file path:
+C:\xampp\htdocs\dre-2018\myGitHub\aniket\src\component\productList\AdminProducts.js (update header in admin)
+C:\xampp\htdocs\dre-2018\myGitHub\aniket\src\component\Home.js (home nav)
---
+++ react.js/v1co-engineering-bug-test-f/src/containers/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import LoginForm from '../components/LoginForm'
+import { Glyphicon } from 'react-bootstrap';
+import '../styles/App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showLoginForm: true,
+      showCheckmark: false
+    };
+    this.handleLogout = this.handleLogout.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin() {
+    this.refs.navbutton.handleLogoutButton();
+    this.setState({ 
+      showLoginForm: true,
+      showCheckmark: true
+    });
+  }
+
+  handleLogout() {
+    this.refs.navbutton.handleLogoutButton();
+    this.setState({
+      showLoginForm: true,
+      showCheckmark: false
+    });
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <Navbar ref='navbutton' handleLogout={this.handleLogout} />
+        <div className={this.state.showLoginForm === true ? '' : 'hide'}>
+          <LoginForm handleLogin={this.handleLogin} />
+        </div>
+        <div className={this.state.showCheckmark === true ? 'text-center mt9x' : 'hide'}>
+          <Glyphicon glyph='glyphicon glyphicon-ok-sign' />
+          <h2>Great work!</h2>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
